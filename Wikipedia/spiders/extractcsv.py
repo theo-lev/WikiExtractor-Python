cbc53d08b8c3ec4a72a2f32049912d96fc3af163
Sadou <sadouship@gmail.com>
2020-11-02 11:44:15 +0100

    Extraction de tableau wikipedia dans un Object Dict

--- Wikipedia/spiders/extractcsv.py
+++ Wikipedia/spiders/extractcsv.py
@@ -3,18 +3,33 @@ import scrapy
 
 class ExtractcsvSpider(scrapy.Spider):
     name = 'extractcsv'
-    allowed_domains = ['https://en.wikipedia.org/wiki/Comparison_between_Esperanto_and_Ido']
-    start_urls = ['http://https://en.wikipedia.org/wiki/Comparison_between_Esperanto_and_Ido/']
-    custom_settings = {
-        'FEED_URI': 'tmp/extractcsv.csv'
-    }
+    allowed_domains = ['wikipedia.org']
+    start_urls = [
+        'https://fr.wikipedia.org/wiki/France#:~:text=Au%201er%20janvier%202018,mer%20et%20en%20Nouvelle%2DCal%C3%A9donie.',
+        'https://en.wikipedia.org/wiki/Comparison_between_Esperanto_and_Ido'
+    ]
+    # custom_settings = {
+    #     'FEED_URI': 'tmp/extractcsv.csv'
+    # }
 
     def parse(self, response):
-        title = response.css('//th').extract()
-
-        for item in zip(title):
-            scraped_info = {
-                'title' : item[0],
-            }
-
-        yield scraped_info
+        tables = response.css('table.wikitable')
+        tableau = {}
+        i = 0
+        for row in tables:
+            tableau[i] = row.css('tr th::text, tr td::text').extract()
+            i+=1
+            yield tableau
+        convertCSV(tableau)
+        #table = response.css['table.wikitable']
+        #print(table.extract())
+        # title = response.css('//th').extract()
+        #
+        # for item in zip(title):
+        #     scraped_info = {
+        #         'title' : item[0],
+        #     }
+        #
+        # yield scraped_info
+def convertCSV(tableau):
+    print(tableau)
