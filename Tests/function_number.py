6293a85ef596e522776f1b74b6e13e036d76de72
clementdepond <clement.depond53@gmail.com>
2020-11-06 15:32:50 +0100

    UPDATE

--- Tests/function_number.py
+++ Tests/function_number.py
@@ -1,14 +1,12 @@
 import os
 
-listeFichiers = os.listdir('../output')
+listeFichiers = os.listdir('../output') #Create a list with all results
 
 def IteratorURL(url):
     i = 0
     for file in listeFichiers:
-        if file[0:len(file)-6] == url:
-            i+=1
-        if file[0:len(file)-7] == url:
+        if file[0:len(file)-6] == url:      #Here, we delete the "_0.csv" of each files and compare with url
+            i+=1                            #If the result is equal to the file, we increment i
+        if file[0:len(file)-7] == url:      #The function search every file in list, it can be otpimized
             i+=1
     return(i)
-
-#IteratorURL('Comparison_of_Asian_national_space_programs')
--- Wikipedia/spiders/convertURL.py
+++ Wikipedia/spiders/convertURL.py
@@ -1,18 +1,17 @@
 import os
 
-
 def getConvertUrl():
-    fileDir = os.path.dirname(os.path.realpath('__file__'))
-    fileName = os.path.abspath(fileDir + '/spiders/wikiurl.txt')
-    listConvertUrl = []
+    fileDir = os.path.dirname(os.path.realpath('__file__'))         #The path of the file
+    fileName = os.path.abspath(fileDir + '/spiders/wikiurl.txt')    #The name of the file
+    listConvertUrl = []                                             #Creation of a list with all the URLs
     totalUrl = 0
-    names = open(fileName).readlines()
+    names = open(fileName).readlines()                              #We read all the lines in wikiurl
     for namePage in names:
 
-        namePage.replace("&", "%26")
+        namePage.replace("&", "%26")                                #A '&' in the URL provock an error
         namePage.replace("Home_Owners'_and_Civic_Associations", "homeowner_associations_and_civic_associations")
 
-        listConvertUrl.append(('https://en.wikipedia.org/wiki/' + namePage).rstrip("\n"))
+        listConvertUrl.append(('https://en.wikipedia.org/wiki/' + namePage).rstrip("\n")) #We delete all the "\n"
         totalUrl += 1
         print(str(totalUrl) + ' lines convert in url.')
     return listConvertUrl
