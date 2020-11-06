7a6a8f192165232325c17b04f8897c767051d23e
Sadou <sadouship@gmail.com>
2020-11-06 14:08:57 +0100

    Test U, fix errors Issues #6

--- Tests/TestExtracteur.py
+++ Tests/TestExtracteur.py
@@ -18,5 +18,35 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(83, IteratorURL('List_of_AMD_graphics_processing_units'))
         self.assertEqual(16, IteratorURL('Comparison_of_programming_languages_(basic_instructions)'))
 
+    def test_link11(self):
+        self.assertEqual(0, IteratorURL('Comparison_of_Axis_&_Allies_games'))
+
+    def test_link12(self):
+        self.assertEqual(0, IteratorURL('Comparison_of_C_Sharp_and_Visual_Basic_.NET'))
+
+    def test_link13(self):
+        self.assertEqual(1, IteratorURL('Comparison_of_Chernobyl_and_other_radioactivity_releases'))
+
+    def test_link14(self):
+        self.assertEqual(0, IteratorURL('Comparison_of_Exchange_ActiveSync_clients'))
+
+    def test_link15(self):
+        self.assertEqual(4, IteratorURL('Comparison_of_Hokkien_writing_systems'))
+
+    def test_link16(self):
+        self.assertEqual(0, IteratorURL('Comparison_of_Home_Owners'+'_and_Civic_Associations'))
+
+    def test_link17(self):
+        self.assertEqual(2, IteratorURL('Comparison_of_IOC,_FIFA,_and_ISO_3166_country_codes'))
+
+    def test_link18(self):
+        self.assertEqual(6, IteratorURL('Comparison_of_Java_and_C++'))
+
+    def test_link19(self):
+        self.assertEqual(6, IteratorURL('Comparison_of_Linux_distributions'))
+
+    def test_link20(self):
+        self.assertEqual(1, IteratorURL('Comparison_of_MD_and_DO_in_the_United_States'))
+
 if __name__ == '__main__':
     unittest.main()
--- Wikipedia/spiders/convertURL.py
+++ Wikipedia/spiders/convertURL.py
@@ -8,6 +8,10 @@ def getConvertUrl():
     totalUrl = 0
     names = open(fileName).readlines()
     for namePage in names:
+
+        namePage.replace("&", "%26")
+        namePage.replace("Home_Owners'_and_Civic_Associations", "homeowner_associations_and_civic_associations")
+
         listConvertUrl.append(('https://en.wikipedia.org/wiki/' + namePage).rstrip("\n"))
         totalUrl += 1
         print(str(totalUrl) + ' lines convert in url.')
