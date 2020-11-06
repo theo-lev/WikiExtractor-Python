55fab27c19f3d7c1d64fde5f3170dcfeb7ce9112
clementdepond <clement.depond53@gmail.com>
2020-11-06 12:09:19 +0100

    iterator function number

--- Tests/function_number.py
+++ Tests/function_number.py
@@ -7,6 +7,8 @@ def IteratorURL(url):
     for file in listeFichiers:
         if file[0:len(file)-6] == url:
             i+=1
+        if file[0:len(file)-7] == url:
+            i+=1
     return(i)
 
-IteratorURL('Comparison_between_Esperanto_and_Ido')
+#IteratorURL('Comparison_of_Asian_national_space_programs')
