8e2194c5c261fda64252086af3f6696657329bd8
theo.lvq <t.lvq22@gmail.com>
2020-11-06 15:08:08 +0100

    close #6

--- Wikipedia/spiders/convertURL.py
+++ Wikipedia/spiders/convertURL.py
@@ -4,14 +4,14 @@ import os
 def getConvertUrl():
     fileDir = os.path.dirname(os.path.realpath('__file__'))
     fileName = os.path.abspath(fileDir + '/spiders/wikiurl.txt')
+
     listConvertUrl = []
     totalUrl = 0
-    names = open(fileName).readlines()
+
+    file = open(fileName, 'r', encoding='utf-8')
+    names = file.readlines()
+
     for namePage in names:
-
-        namePage.replace("&", "%26")
-        namePage.replace("Home_Owners'_and_Civic_Associations", "homeowner_associations_and_civic_associations")
-
         listConvertUrl.append(('https://en.wikipedia.org/wiki/' + namePage).rstrip("\n"))
         totalUrl += 1
         print(str(totalUrl) + ' lines convert in url.')
